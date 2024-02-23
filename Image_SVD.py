39638dc9553cf7654a96ff72489ed6453a6828eb
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2024-02-23 10:58:51 -0500

    function to compress the matrix of a single channel

--- Image_SVD.py
+++ Image_SVD.py
@@ -11,3 +11,14 @@ def openImage(imagePath):
     aBlue = im[:, :, 2]
 
     return [aRed, aGreen, aBlue, imOrig]
+
+#compress the matrix of a single channel depending of singular values limit
+def compressSingleChannel(channelDataMatrix, singularValuesLimit):
+    uChannel, sChannel, vhChannel = numpy.linalg.svd(channelDataMatrix)
+    aChannelCompressed = numpy.zeros((channelDataMatrix.shape[0], channelDataMatrix.shape[1]))
+    k = singularValuesLimit
+
+    leftSide = numpy.matmul(uChannel[:, 0:k], numpy.diag(sChannel)[0:k, 0:k])
+    aChannelCompressedInner = numpy.matmul(leftSide, vhChannel[0:k, :])
+    aChannelCompressed = aChannelCompressedInner.astype('uint8')
+    return aChannelCompressed
