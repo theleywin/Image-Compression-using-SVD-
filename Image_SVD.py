0ed26330ce84569de45d66490119cbc0853a8605
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2024-02-23 10:47:10 -0500

    openImage function created

---
+++ Image_SVD.py
@@ -0,0 +1,13 @@
+import numpy
+from PIL import Image
+
+# open the image and return 3 matrices, each corresponding to one channel (R, G, B channels)
+def openImage(imagePath):
+    imOrig = Image.open(imagePath)
+    im = numpy.array(imOrig)
+
+    aRed = im[:, :, 0]
+    aGreen = im[:, :, 1]
+    aBlue = im[:, :, 2]
+
+    return [aRed, aGreen, aBlue, imOrig]
